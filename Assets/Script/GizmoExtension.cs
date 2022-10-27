c4ee4a9ded71cc8c19b0df90a255dd90f336a731
Jean-LoupMARTIN <jlmc78@outlook.fr>
2022-10-27 18:32:36 +0200

    Import

---
+++ Assets/Script/GizmoExtension.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class GizmoExtension
+{
+    static public void DrawLine(Vector3 p1, Vector3 p2, float width, Color color)
+    {
+        Handles.DrawBezier(p1, p2, p1, p2, color, null, width);
+    }
+
+    static public void DrawSegment(Vector3 p1, Vector3 p2, float width, float extremitySize, Quaternion extremityRotation, Color color)
+    {
+        Handles.DrawBezier(p1, p2, p1, p2, color, null, width);
+
+        Vector3 p11 = p1 + extremityRotation * Vector3.up * extremitySize / 2;
+        Vector3 p12 = p1 - extremityRotation * Vector3.up * extremitySize / 2;
+        Handles.DrawBezier(p11, p12, p11, p12, color, null, width);
+
+        Vector3 p21 = p2 + extremityRotation * Vector3.up * extremitySize / 2;
+        Vector3 p22 = p2 - extremityRotation * Vector3.up * extremitySize / 2;
+        Handles.DrawBezier(p21, p22, p21, p22, color, null, width);
+    }
+
+    static public void DrawArc(Vector3 center, Vector3 start, Vector3 end, float radius, int resolution, float width, Color color)
+    {
+        Vector3 up = Vector3.Cross(start, end);
+        Quaternion startRot = Quaternion.LookRotation(start, up);
+        Quaternion endRot   = Quaternion.LookRotation(end,   up);
+
+        for (int i = 1; i < resolution; i++)
+        {
+            float progress1 = (float)(i-1) / (resolution - 1);
+            float progress2 = (float) i    / (resolution - 1);
+
+            Vector3 p1 = center + Quaternion.Lerp(startRot, endRot, progress1) * Vector3.forward * radius;
+            Vector3 p2 = center + Quaternion.Lerp(startRot, endRot, progress2) * Vector3.forward * radius;
+
+            DrawLine(p1, p2, width, color);
+        }
+
+        DrawLine(center, center + startRot * Vector3.forward * radius, width, color);
+        DrawLine(center, center + endRot   * Vector3.forward * radius, width, color);
+    }
+}
---
+++ Assets/Script/IK.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class IK : MonoBehaviour
+{
+    [SerializeField] Transform[] knees;
+    [SerializeField] Transform feet;
+    [SerializeField] Transform target;
+
+    List<(float angle, float dist)> anglesDistances = new List<(float, float)>();
+    [SerializeField] float angleStart = 0;
+    [SerializeField] float angleEnd = 120;
+    [SerializeField] float angleStep = 5;
+
+    void OnValidate()
+    {
+        CacheAnglesDistances();
+    }
+
+    void Awake()
+    {
+        CacheAnglesDistances();
+    }
+
+    void Update()
+    {
+        MatchTarget();
+    }
+
+    void CacheAnglesDistances()
+    {
+        anglesDistances.Clear();
+
+        if (angleStep <= 0 || !feet)
+            return;
+
+        for (float angle = angleStart; angle <= angleEnd; angle += angleStep)
+            anglesDistances.Add((angle, DistanceToFeet(angle)));
+
+        //PrintAnglesDistances();
+    }
+
+    void PrintAnglesDistances()
+    {
+        print("\nPrintAnglesDistances\n");
+
+        foreach ((float angle, float dist) e in anglesDistances)
+            print("Angle : " + e.angle + "\t\tDist : " + e.dist);
+    }
+
+    float DistanceToFeet(float angle)
+    {
+        SetAngle(angle);
+
+        return (transform.position - feet.position).magnitude;
+    }
+
+    void SetAngle(float angle)
+    {
+        foreach (Transform knee in knees)
+            knee.localRotation = Quaternion.Euler(angle, 0, 0);
+    }
+
+    void MatchTarget()
+    {
+        if (!feet || !target)
+            return;
+
+        Vector3 toTarget = target.position - transform.position;
+        float distToTarget = toTarget.magnitude;
+        float angle = FindAngle(distToTarget);
+        SetAngle(angle);
+        transform.forward = toTarget;
+        transform.rotation *= Quaternion.Euler(-Vector3.Angle(transform.forward, feet.position - transform.position), 0, 0);
+    }
+
+    float FindAngle(float dist)
+    {
+        if (anglesDistances.Count == 0)
+            return 0;
+
+        for (int i = 0; i < anglesDistances.Count; i++)
+        {
+            (float angle, float dist) e1 = anglesDistances[i];
+
+            if (e1.dist < dist)
+            {
+                if (i == 0)
+                    return e1.angle;
+
+                (float angle, float dist) e2 = anglesDistances[i-1];
+
+                return Mathf.Lerp(e1.angle, e2.angle, Mathf.InverseLerp(e1.dist, e2.dist, dist));
+            }
+        }
+
+        return anglesDistances.Last().angle;
+    }
+}
